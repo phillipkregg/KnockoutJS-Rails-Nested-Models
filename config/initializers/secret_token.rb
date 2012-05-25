4ffebc15e57bc277ef70a26a1aa62422615a8a5d
Your Name <phillipkregg@gmail.com>
2012-05-25 10:49:59 -0500

    Initial Commit - added Knockout.js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeschoolKnockout::Application.config.secret_token = '5a6b4f602e075f5a4a25af605c479b950bb8b89fd3f2e90707e20f7eef5252d6a1388b03ef0fcb09fd8b83d58ed77b6d5ecde17e0769cb6bc5281efd1df19e36'
